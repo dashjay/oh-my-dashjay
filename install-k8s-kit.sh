f5c711587ebcda156aa99aeb2bbce1ed0b2c84bf
DashJay <45532257+dashjay@users.noreply.github.com>
2021-02-19 16:44:31 +0800

    Create install-k8s-kit.sh

---
+++ install-k8s-kit.sh
@@ -0,0 +1,11 @@
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-get install -y docker.io
+sudo swapoff -a
+sudo sed -i "s/\/swap/#\/swap/g" /etc/fstab
+ufw disable
